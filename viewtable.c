ea9274078b2f3450cfadf36d4409530355c0e9e5
cosney <cosney@cosneys-iMac.local>
2020-04-24 19:34:28 +0300

    4x4 working well

--- viewtable.c
+++ viewtable.c
@@ -6,30 +6,12 @@
 /*   By: cosney <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/24 03:35:10 by cosney            #+#    #+#             */
-/*   Updated: 2020/04/24 04:47:46 by cosney           ###   ########.fr       */
+/*   Updated: 2020/04/24 19:33:39 by cosney           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	viewtable(char **argv, char board[6][6])
+void	view_up(char **argv, char board[6][6], int i, int c)
 {
-	int		i;
-	int		c;
-
-	i = 0;
-	c = 1;
-	while (i < 8)
-	{
-		board[0][c] = argv[1][i];
-		i = i + 2;
-		c++;
-	}
-	c = 1;
-	while (i < 16)
-	{
-		board[5][c] = argv[1][i];
-		i = i + 2;
-		c++;
-	}
 	c = 1;
 	while (i < 24)
     {
@@ -55,6 +37,29 @@ void	viewtable(char **argv, char board[6][6])
 		}
 		i++;
 	}
+}
+
+void	viewtable(char **argv, char board[6][6])
+{
+	int		i;
+	int		c;
+
+	i = 0;
+	c = 1;
+	while (i < 8)
+	{
+		board[0][c] = argv[1][i];
+		i = i + 2;
+		c++;
+	}
+	c = 1;
+	while (i < 16)
+	{
+		board[5][c] = argv[1][i];
+		i = i + 2;
+		c++;
+	}
+	view_up(argv, board, i, c);
 	board[0][0] = '.';
 	board[5][5] = '.';
 	board[0][5] = '.';
